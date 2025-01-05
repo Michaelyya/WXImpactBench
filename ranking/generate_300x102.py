54a55f47ee943dd05f79589a13865f1e96e6c504
XiandaDu <283149519du@gmail.com>
2025-01-05 13:40:17 -0500

    feat: created size 100 ranking pool and its random generation script

---
+++ ranking/generate_300x102.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import random
+
+RANDOM_SEED = 1510
+random.seed(RANDOM_SEED)
+
+input_file = "passages_and_queries.csv"
+output_file = "query_and_100_passages.csv"
+
+data = pd.read_csv(input_file)
+output_data = []
+
+for idx, row in data.iterrows():
+    correct_passage = row['Text']
+    query = row['Generated_Query']
+
+    all_passages = data['Text'].tolist()
+    all_passages.remove(correct_passage)
+    distractors = random.sample(all_passages, 99)
+    
+    passages = distractors + [correct_passage]
+    random.shuffle(passages)
+    
+    new_row = {
+        "id": idx + 1,
+        "query": query,
+    }
+    for i, passage in enumerate(passages):
+        new_row[f"passage_{i+1}"] = passage
+    
+    output_data.append(new_row)
+
+
+output_df = pd.DataFrame(output_data)
+output_df.to_csv(output_file, index=False)
